cb0eb704bcf2c0caf92e20300edc43d197659e0f
szalmasi.nikol <szalmasi.nikol@ckik.hu>
2024-04-25 16:29:58 +0200

    beolvasas mar nem 3 def, valtozonevek szebbek, dokumentacio

--- projekt.py
+++ projekt.py
@@ -1,19 +1,7 @@
 from random import randint
 
-def sima(iz,velemeny,ar):
-    fr = open("be1.txt", "r", encoding ="UTF=8")
-    sor = fr.readline().strip()
-    while sor != "":
-        darab = sor.split()
-        iz.append(darab[0])
-        velemeny.append(float(darab[1]))
-        ar.append(int(darab[2]))
-        sor = fr.readline().strip()
-    fr.close()
 
-
-def mentes(iz,velemeny,ar):
-    fr = open("be2.txt", "r", encoding ="UTF=8")
+def darabolas(iz, velemeny, ar, fr):
     sor = fr.readline().strip()
     while sor != "":
         darab = sor.split()
@@ -27,11 +15,14 @@ def mentes(iz,velemeny,ar):
 def beolvas(iz,velemeny,ar):
     k = input("Melyik fájl?(sima/laktózmentes): ")
     if k == "sima":
-        sima(iz,velemeny,ar)
+        fr = open("be1.txt", "r", encoding ="UTF=8")
+        darabolas(iz, velemeny, ar, fr)
     elif k == "laktózmentes":
-        mentes(iz,velemeny,ar)
+        fr = open("be2.txt", "r", encoding ="UTF=8")
+        darabolas(iz, velemeny, ar, fr)
     else:
        beolvas(iz,velemeny,ar)
+
 
 def finomfagyik(velemeny):
     ossz = 0
@@ -41,15 +32,16 @@ def finomfagyik(velemeny):
             ossz += 1
     return ossz
 
-def finomfagyik_list(iz, velemeny, otratinglist):
+
+def finomfagyik_list(iz, velemeny, finomlista):
     n = len(velemeny)
     for i in range(n):
         if velemeny[i] > 4.5:
-            otratinglist.append(iz[i])
-    return otratinglist        
+            finomlista.append(iz[i])
+    return finomlista       
   
 
-def draga(ar,iz):
+def dragafagyi(ar,iz):
     n = len(ar)
     maxi = 0
     for i in range(1,n):
@@ -58,14 +50,15 @@ def draga(ar,iz):
     return iz[maxi]
         
      
-def atlag_rating(velemeny):
+def atlag_velemeny(velemeny):
     s = 0
     n = len(velemeny)
     for i in range(n):
         s += velemeny[i]
     atlag = s / n
     return atlag
-    
+
+
 def novekvo(ar):
     n = len(ar)
     for i in range(n):
@@ -74,7 +67,8 @@ def novekvo(ar):
                 ar[j], ar[j+1] = ar[j+1], ar[j]
     return ar
 
-def intolcso(iz,ar):
+
+def intolcso(ar):
     n = len(ar)
     mini = 0
     for i in range(1, n):
@@ -110,51 +104,51 @@ def hozzairas():
     print("Véleményét rögzítettük!")
     fa.close()
 
+
 def akcio(legolcsobb):
     r = randint(10, 30)
     akciosar = int(legolcsobb - (r / 100 * legolcsobb))
     return akciosar
+
 
 def nyil(novekvok):
     for i in range(len(novekvok)-1):
         print(novekvok[i], "=>", end=(" "))
     print(novekvok[len(novekvok)-1])
 
-def statisztika(iz,velemeny, ar, olcsofagyik, otratinglist):
+
+def statisztika(iz,velemeny, ar, olcsofagyik, finomlista):
     print()
     print("Bim-Bam Fagyizó információk:")
     # F1
     print(f"1.) Ennyi 4,5 csillag fölötti fagyi közül választhat: {finomfagyik(velemeny)}")
     # F2
-    otratingfagyi = finomfagyik_list(iz, velemeny, otratinglist)
-    print(f"2.) 4,5 csillag fölötti fagylaltjaink:", *otratingfagyi)
+    finomfagyi = finomfagyik_list(iz, velemeny, finomlista)
+    print(f"2.) 4,5 csillag fölötti fagylaltjaink:", *finomfagyi)
     # F3
-    print("3.) A legdrágább fagyink:", draga(ar,iz))
+    print("3.) A legdrágább fagyink:", dragafagyi(ar,iz))
     # F4
-    print(f"4.) A fagyizó átlag véleménye: {round(atlag_rating(velemeny), 1)} ")
+    print(f"4.) A fagyizó átlag véleménye: {round(atlag_velemeny(velemeny), 1)} ")
     # F5
-    legolcsobb = intolcso(iz,ar)
+    legolcsobb = intolcso(ar)
     olcsofagyik = olcsoklista(ar, iz, legolcsobb, olcsofagyik)
     print("5.) A legolcsóbb fagylaltjaink:", *olcsofagyik)  
     # F6
     print("6.) Mai akciónkkal a legolcsóbb fagylaltjaink/fagylaltunk ára:", akcio(legolcsobb))
     #F7
     novekvok = novekvo(ar)
-    print("7.) Fagylaltjaink ára növekvő sorrendben: ", end=(" "))
+    print("7.) Fagylaltjaink ára növekvő sorrendben:", end=(" "))
     nyil(novekvok)
-    
-    
-        
-        
+         
 
 def main():
     iz,velemeny, ar= [], [], []
     beolvas(iz,velemeny,ar)
     kerdes = iras_olvas()
     olcsofagyik = []
-    otratinglist = []
+    finomlista = []
     if kerdes == "statisztika":
-        statisztika(iz,velemeny, ar, olcsofagyik,otratinglist)
+        statisztika(iz,velemeny, ar, olcsofagyik, finomlista)
     else:
         hozzairas()
         
